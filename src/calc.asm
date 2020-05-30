25fd97a15361efed51c38ff7bc6df354aae730c9
kzynn <herbstigel@gmx.net>
2020-05-31 01:17:05 +0200

    Improve register usage

--- src/calc.asm
+++ src/calc.asm
@@ -5,9 +5,8 @@
 ; (calc field at 78h)
 ;
 ; r7 - row
-; r6 - col
-; r5 - col bit
-; r4 - sum of alive surrounding cells
+; r6 - col bit
+; r5 - sum of alive surrounding cells
 ; ----------------------
 
 cseg at 0h
@@ -27,74 +26,69 @@ init:
 
 calc:
 	mov r7, #8d		; init row count with 8, start with first row
-		loop_cols:
+	loop_cols:
 		; loop trough cols
-		
-			mov r6, #08d	; init col count with 7
-			mov r5, #01h	; init col bit with 1
-			loop_rows:
-				; loop trough rows
-				dec r6					; decrement col count
-				mov a, r5				; shift col bit
-				rr a
-				mov r5, a
-				mov r4, #0d	; reset curr cell count
+		mov r6, #01h	; init col bit with 1
+		loop_rows:
+			; loop trough rows
+			mov a, r6	; shift col bit
+			rr a
+			mov r6, a
+			mov r5, #0d	; reset surrounding cell count
 
-				; do things
-				call calc_cell
-				call set_cell
+			; do things
+			call calc_cell
+			call set_cell
 
-				;cjne r5, #01h, loop_rows ; end loop if col bit has passed calculation for lowest bit
-				cjne r6, #0d, loop_rows ; end loop if r7=colcount reaches 0
-
-		inc r7					 ; increment row count
-		cjne r7, #16d, loop_cols ; end loop if r7=rowcount reaches 7
-		ajmp display
+			cjne r6, #01h, loop_rows ; end loop if col bit has passed calculation for lowest bit
+	inc r7					 ; increment row count
+	cjne r7, #16d, loop_cols ; end loop if r7=rowcount reaches 7
+	ajmp display
 
 calc_cell:
 	; calc surrounding cells alive
 	; row above
 	dec r7
-	mov a, r5
+	mov a, r6
 	rl a
-	mov r5, a
+	mov r6, a
 	call check_alive
-	mov a, r5
+	mov a, r6
 	rr a
-	mov r5, a
+	mov r6, a
 	call check_alive
-	mov a, r5
+	mov a, r6
 	rr a
-	mov r5, a
+	mov r6, a
 	call check_alive
 	; cells left and right
 	inc r7
 	call check_alive
-	mov a, r5
+	mov a, r6
 	rl a
 	rl a
-	mov r5, a
+	mov r6, a
 	call check_alive
 	; cells below
 	inc r7
 	call check_alive
-	mov a, r5
+	mov a, r6
 	rr a
-	mov r5, a
+	mov r6, a
 	call check_alive
-	mov a, r5
+	mov a, r6
 	rr a
-	mov r5, a
+	mov r6, a
 	call check_alive
 	; clean up row and col bit registers
 	dec r7
-	mov a, r5
+	mov a, r6
 	rl a
-	mov r5, a
+	mov r6, a
 	ret
 
 check_alive:
-	; checks if a cell is alive and if so increments r4
+	; checks if a cell is alive and if so increments r5
 
 	; calc current row offset
 	; modulo operation to prevent -1 and 8 as row
@@ -107,14 +101,14 @@ check_alive:
 	add a, r1
 	mov r0, a
 	mov a, @r0	; mov current row in a
-	anl a, r5	; and operation for selecting specific cell
+	anl a, r6	; and operation for selecting specific cell
 	jz skip_if_cell_dead
-	inc r4
+	inc r5
 	skip_if_cell_dead:
 	ret
 
 set_cell:
-	; check value in r4 and set new value for cell in new field
+	; check value in r5 and set new value for cell in new field
 	mov a, r7 	; move begin of current array in acc
 	mov b, #08d
 	div ab 		; calc modulo, result in b register
@@ -131,14 +125,14 @@ set_cell:
 	add a, #78h
 	mov r0, a	; store new memory location in r0
 
-	cjne r4, #03d, cell_is_dead
+	cjne r5, #03d, cell_is_dead
 	; cell is alive
 	mov a, @r0
-	orl a, r5
+	orl a, r6
 	mov @r0, a
 	ret
 	cell_is_dead:
-	mov a, r5
+	mov a, r6
 	cpl a		; invert sequence
 	mov r2, a	; move inverted to r2
 	mov a, @r0
